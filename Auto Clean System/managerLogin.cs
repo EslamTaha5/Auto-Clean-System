53e2110f20fbe2689053141ce06661d89209c6fd
Mahmoud Saber <rodomahmoud121@gmail.com>
2025-05-10 14:09:40 +0300

    Initial commit with folder structure

---
+++ Auto Clean System/managerLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auto_Clean_System
+{
+    public partial class managerLogin : Form
+    {
+        public managerLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            RoleSelection SelectRole = new RoleSelection();
+            SelectRole.Show();
+            this.Hide();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            managerDashboard Manager = new managerDashboard();
+            Manager.Show();
+            this.Hide();
+        }
+    }
+}
